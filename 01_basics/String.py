7dbe28c7089cccd7deb95b9cab4524a3869ab20a
nayan <www.xil187@gmail.com>
2024-09-05 00:04:00 +0530

    Strings done

---
+++ 01_basics/String.py
@@ -0,0 +1,63 @@
+chai = "Masala chai"
+# slice_chai = chai[0]
+# print(slice_chai)
+# printed first character og String
+
+# slicing
+# print(chai[0:6])
+
+# num_list = "0123456789"
+# print(num_list[:])
+# print(num_list[3:])
+
+# print(num_list[0:7:2])
+
+# print(num_list[0:7:3])
+
+# print(chai.lower())
+# print(chai.upper())
+
+# newChai = "      Masala chai  "
+# print(newChai.strip())
+
+# newchai = "Lemon chai"
+# print(newchai.replace("Lemon", "Ginger"))
+
+# newchai = "masala, mint, Ginger, Lemon"
+# print(chai.split(", "))
+
+# newChai = "Masala chai"
+# print(chai.find("chai"))
+# -1 means not available
+
+# newChai = "Masala chai chai chai"
+# print(newChai.count("chai"))
+# # 3
+
+# newChai = "Masala"
+# quantity = 2
+# order = "I ordered {} cups of {} chai."
+# print(order.format(quantity, newChai))
+
+# chai_variety = ["Lemon", "Masala", "Ginger"]
+# print(chai_variety)
+
+# print(" ".join(chai_variety))
+
+# print(len(chai_variety))
+# # 3
+
+# for letter in chai:
+#     print(letter)
+
+# new_chai = "He said, \"Masala chai is awesome\" "
+# print(new_chai)
+# new_chai = "Masala \nchai"
+# print(new_chai)
+
+# new_chai=r"Masala\nchai"
+# print(new_chai)
+
+# chai = "Masala chai"
+# print("Masala" in chai)
+# True
