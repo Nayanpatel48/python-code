4b3701d5af02e8bc9521071256b8fc11d7a4144d
nayan <www.xil187@gmail.com>
2024-09-04 10:21:57 +0530

    python internal working

--- 01_basics/hello_chai.py
+++ 01_basics/hello_chai.py
@@ -3,3 +3,6 @@ print("Chai aur python.")
 #notes
 #byte code is not machine code
 #Byte code is the python specific interpetation
+
+#In python the data type of any value in the memory will remain in this memeory only
+#There no data type of variable
